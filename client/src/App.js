ca9baf23ae61599d68ed4f29f5606fbb8ea44913
unknown <brninfotec>
2024-05-09 18:22:55 +0530

    add components

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Ipl from './components/Ipl';
+
+function App() {
+  return (
+    <div className="App">
+      <Ipl/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Ipl.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ipl() {
+  return (
+    <div>
+      <h1>IPL 2024</h1>
+    </div>
+  )
+}
+
+export default Ipl
