4da987ac1de4c271b5e09cb3def1f95fb9e16667
unknown <brninfotec>
2024-05-10 17:57:02 +0530

    wertyu

--- client/src/components/Srh.js
+++ client/src/components/Srh.js
@@ -7,10 +7,10 @@ function Srh() {
     <div className='body'>
     <div className='container'>
    <div className='logo'>
-     <img className='img' src="https://www.iplt20.com/assets/images/IPL_LOGO_CORPORATE_2024.png"></img>
+     <img className='img' src="https://www.iplt20.com/assets/images/IPL_LOGO_CORPORATE_2024.png" alt=''></img>
    </div>
    <div className="background">
-     <img className='img2' src="https://i.postimg.cc/W1hktGYn/srhlogo.png"></img>
+     <img className='img2' src="https://i.postimg.cc/W1hktGYn/srhlogo.png" alt=''></img>
    </div>
    <div className="heading">
      <h1 className="head" style={{color:'orangered'}}>Sunrisers Hyderabad</h1>
@@ -21,7 +21,7 @@ function Srh() {
    <div className="image">
      <img className='img3'
       style={{width:"40%", height:"35%", marginLeft:"60%"}}
-        src="https://i.postimg.cc/BQhFqYpb/srh.png"></img>
+        src="https://i.postimg.cc/BQhFqYpb/srh.png" alt=''></img>
    </div>
    <div className="navbar">
      <ul>
