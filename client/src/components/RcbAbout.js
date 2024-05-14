39502b23610d5a8c3ba152558e73153ff1529b72
stonerram <stonerram488@gmail.com>
2024-05-14 13:50:05 +0530

    saved

--- client/src/components/RcbAbout.js
+++ client/src/components/RcbAbout.js
@@ -6,11 +6,10 @@ function RcbAbout() {
              
     <div style={{backgroundColor:"lightblue", width:"100%", height:"100%", marginTop:"-2%"}}>
       
-<<<<<<< HEAD
+
          <div className='container'>
          <div className='headingAbout'>
       <h2 className="headAbout"> Royal Challengers Bangalore (RCB)</h2>
-=======
          
 
 
@@ -21,7 +20,7 @@ function RcbAbout() {
         <Link className='navbar2'  to="/Rcb" style={{color:"black",position:"relative", right:"700px",top:'20px'}}>Back</Link>
        
          <h2 className="headAbout"> Royal Challengers Bangalore (RCB)</h2>
->>>>>>> 63b71f836cab76d14d413b52db58a7f3b62f630d
+
       <p className='paraAbout'>Welcome to the official page of Royal Challengers Bangalore (RCB)! We are one of the most exciting teams in the IPL, known for our explosive batting lineup and passionate fan base.</p>
       <p className='paraAbout'>Founded in 2008, RCB has always aimed to entertain cricket fans around the world with our aggressive style of play. Led by our charismatic captain, we strive for excellence on and off the field.</p>
       <h3 className="headAbout">Our Mission</h3>
@@ -42,6 +41,7 @@ function RcbAbout() {
     </div>
     </div>
     </div>
+    </div></div>
   )
 }
 
